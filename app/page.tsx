b6340c47f9afbcbf7829b75feaedef1c3e47ed0a
v0agent <it+v0agent@vercel.com>
2026-05-28 22:14:12 +0000

    Initial commit from v0
    #VERCEL_SKIP

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button"
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-background">
+      <Button>Clique aqui</Button>
+    </main>
+  )
+}
